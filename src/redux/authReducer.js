f36084f3192d028eb2a4798cb6589eedaca64b30
Chris <cabiediger@gmail.com>
2019-09-08 17:59:08 -0400

    cleaned up authReducer

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,15 +1,9 @@
 
 const initialState = {
     user_data: null,
-    username: '',
-    id: '',
-    first_name: ''
 }
 
 const HANDLE_SESSION_DATA = 'HANDLE_SESSION_DATA' 
-const HANDLE_USERNAME = 'HANDLE_USERNAME'
-const HANDLE_ID = 'HANDLE_ID'
-const HANDLE_FIRST_NAME = 'HANDLE_FIRST_NAME'
 
 export const handleSessionData = data => {
     return {
@@ -18,26 +12,7 @@ export const handleSessionData = data => {
     }
 }
 
-export const handleUsername = username => {
-    return {
-        type: HANDLE_USERNAME,
-        payload: username
-    }
-}
 
-export const handleId = id => {
-    return {
-        type: HANDLE_ID,
-        payload: id
-    }
-}
-
-export const handleFirstname = firstname => {
-    return {
-        type: HANDLE_FIRST_NAME,
-        payload: firstname
-    }
-}
 
 export default function authReducer(state=initialState,action) {
     const { type, payload } = action
