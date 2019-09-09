f8ac59f5e7c184b29df81cef3146e1796a5b74c9
Chris <cabiediger@gmail.com>
2019-09-09 12:40:47 -0400

    add Dashboard, add conditional session rendering

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
-import { HashRouter as Router } from 'react-router-dom'
-import routes from './routes'
+import React, { useEffect } from 'react';
+
+import Register from './Components/auth/Register';
+import Login from './Components/auth/Login';
+import Dashboard from './Components/Dashboard'
+
+import { HashRouter as Router } from 'react-router-dom';
+import routes from './routes';
+
+import { connect } from 'react-redux'
+import Axios from 'axios'
+
 import './App.css';
 
-function App() {
+function App(props) {
+
+  useEffect(() => {
+    Axios
+      .get('/auth/session')
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
+  console.log(props)
   return (
     <Router>
       
       {routes}
 
+      {
+        !props.user_data ?
+        <Login />
+        :
+        <Dashboard />
+      }
+
     </Router>
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  const { user_data } = state
+  return { user_data }
+}
+
+export default connect(mapStateToProps)(App);
